2bb81214c15b45c1cf5b631123f660c8046f1ee9
Mcrouse <matthew.crouse@temple.edu>
2021-03-31 00:17:54 -0400

    added service-worker.js and updated idb.js

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+const APP_PREFIX = "Budget-Tracker";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const DATA_CACHE_NAME = "data-cache-version_01";
+
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/style.css",
+  "./js/index.js",
+  "./js/idb.js",
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});

c935817168273cb14689d557cec0152ee6f1963e
Mcrouse <matthew.crouse@temple.edu>
2021-03-31 21:41:14 -0400

    updated service worker with activate and fetch functions

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,17 @@
-const APP_PREFIX = "Budget-Tracker-";
+const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const DATA_CACHE_NAME = "data-cache-version_01";
 
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./css/style.css",
-  "./js/index.js",
-  "./js/idb.js",
-//   "./manifest.json",
+  "/",
+  "/js/idb.js",
+  "/js/index.js",
+  "/css/styles.css",
 ];
 
+//installs service worker 
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -20,3 +20,42 @@ self.addEventListener("install", function (e) {
     })
   );
 });
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
+});
+
+
